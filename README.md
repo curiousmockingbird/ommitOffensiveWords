b7bfb07b1cbcf03870e22bbce24610637c396e72
Harold Mesa <harold.mesa@icloud.com>
2022-08-16 16:34:26 -0500

    Add UI Logic and fix bug with number of count occurrences of word in text.WIP ommit offensive words function

--- README.md
+++ README.md
@@ -52,6 +52,15 @@ Code:
     numberOfOccurrencesInText(word, text);
 Expected Output: 1
 
+Test: 
+    "If an empty string is passed in as a word, it should return 0."
+Code:
+    const word = "";
+    const text = "red RED Red!";
+    numberOfOccurrencesInText(word, text);
+Expected Output: 0
+
+////
 Describe: omitOffensiveWordsInText()
 Test:
     "It should omit offensive words."
--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,34 @@
 // Business Logic
-
+// Business inteface
 function wordCounter(text) {
-    // 
     if (text.trim().length === 0) {
-        return 0;
-    };
-    // 
+      return 0;
+    }
     let wordCount = 0;
     const textArray = text.split(" ");
-    textArray.forEach(function (elemnt) {
-        if (!Number(elemnt)) {
-            wordCount++
-        };
+    textArray.forEach(function(element) {
+      if (!Number(element)) {
+        wordCount++;
+      }
     });
     return wordCount;
-}
-
-function numberOfOccurrencesInText(word, text) {
-
-    if (word === text) {
-        return 1;
+  }
+  
+  function numberOfOccurrencesInText(word, text) {
+    if (word.trim().length === 0){
+        return 0
     };
 
-    return 0;
+    const textArray = text.split(" ");
+    let wordCount = 0;
+    textArray.forEach(function(element) {
+      if (element.toLowerCase().includes(word.toLowerCase())) {
+        wordCount++;
+      }
+    });
 
-
-}
+    return wordCount;
+  }
 
 function omitOffensiveWordsInText(offensiveWord, text) {
     const textArray = text.split(" ");
@@ -35,7 +38,21 @@ function omitOffensiveWordsInText(offensiveWord, text) {
             alert("Hey, dont use bad words in your text!");
         }
     });
-
-
 }
+
+// UI Logic
+
+function handleFormSubmission(event) {
+    event.preventDefault();
+    const passage = document.getElementById("text-passage").value;
+    const word = document.getElementById("word").value;
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    document.getElementById("total-count").innerText = wordCount;
+    document.getElementById("selected-count").innerText = occurrencesOfWord;
+  }
+  
+  window.addEventListener("load", function() {
+    document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+  });
 
